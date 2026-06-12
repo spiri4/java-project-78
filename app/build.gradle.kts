45a4569ba9586641bffa9a5322a731e92cd0ab70
spiri4 <spiritos@list.ru>
2026-06-12 17:40:48 +0300

    init commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    application
+    id("org.sonarqube") version "7.2.3.7755"
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
